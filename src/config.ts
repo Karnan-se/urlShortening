034e1b68b2210bc9b8bb8c30af6d9e307971fdf7
karnan <karnan.se@gmail.com>
2025-04-20 15:59:35 +0530

    changed is configration for the Hosting

--- src/config.ts
+++ src/config.ts
@@ -10,8 +10,8 @@ export const ConfigKeys = {
     NODE_ENV:process.env.NODE_ENV!,
     ACCESS_TOKEN_EXPIRES_IN :15 * 60 * 1000,
     REFRESH_TOKEN_EXPIRES_IN : 7 * 24 * 60 * 60 * 1000,
-    CLIENT_ORGIN : process.env.NODE_ENV == "development" ? "http://localhost:5173" : "",
+    CLIENT_ORGIN : process.env.NODE_ENV == "development" ? "http://localhost:5173" : "https://url-shortening-react.vercel.app",
     Email : process.env.Email,
     Mail_password : process.env.Mail_password,
-    BASE_URL: process.env.NODE_ENV == "development" ? "http://localhost:3000" : ""
+    BASE_URL: process.env.NODE_ENV == "development" ? "http://localhost:3000" : "https://urlshort.vingle.shop"
 }
